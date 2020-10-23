47f6dc4e2dad3d0d1b9ea13d1e57e4aa35e185b2
FuiOcloo <fui.ocloo@amalitech.org>
2020-10-23 14:33:31 +0000

    assignmnrt1

---
+++ assignment.js
@@ -0,0 +1,15 @@
+let userInput = 20;
+
+let result;
+
+result = userInput + 18;
+
+result = userInput - 2;
+
+result = userInput * 2;
+
+result = userInput / 2;
+
+alert(result);
+
+alert(userInput);
