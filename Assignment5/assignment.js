ac12baa99aeae90f31633b7484d444298c63c7c5
FuiOcloo <fui.ocloo@amalitech.org>
2020-11-01 21:16:05 +0000

    assignment 5

---
+++ Assignment5/assignment.js
@@ -0,0 +1,15 @@
+const tab1stSelection = document.getElementById('task-1');
+const tab2ndSelection = document.querySelector('li');
+
+tab1stSelection.style.color = 'white';
+tab1stSelection.style.backgroundColor = 'black';
+
+
+const title1stSelection = document.querySelector('title');
+const title2ndSelection = document.head.querySelector('title');
+
+title1stSelection.textContent = 'Assignment solved';
+
+
+const h1 = document.getElementsByTagName('h1');
+h1[0].textContent = 'Assignment solved';
